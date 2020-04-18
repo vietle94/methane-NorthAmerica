8a6997089370968783f885be72e247ff3b2c6488
vietle <viet.letranhoang@gmail.com>
2020-04-18 12:16:58 +0300

    overlay states color

--- project.py
+++ project.py
@@ -6,6 +6,7 @@ import h5py
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+import cartopy.io.shapereader as shpreader
 
 # %%
 # List all data files path
@@ -41,15 +42,8 @@ fig.subplots_adjust(hspace=0.3)
 
 # %% Plot map
 fig, ax = plt.subplots(figsize=(16, 9), subplot_kw={'projection': ccrs.PlateCarree()})
-vmin = 1500  # exclude all value <1500, but the aim is to exclude 0
-vmax = 2000
-levels = np.linspace(vmin, vmax, 20)
-contourf_ = ax.contourf(df['longitude'],
-                        df['latitude'],
-                        df['xch4_biascorrected_qa_surf_albedo_filtered'],
-                        cmap='jet', vmin=vmin, vmax=vmax, levels=levels)
+
 ax.set_extent([-130, -60, 24, 52], crs=ccrs.PlateCarree())
-cbar = fig.colorbar(contourf_, ax=ax, orientation='horizontal', fraction=0.05)
 ax.add_feature(cfeature.BORDERS)
 ax.add_feature(cfeature.STATES, linewidth=0.5)
 ax.coastlines()
@@ -61,5 +55,31 @@ gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
 gl.yformatter = LATITUDE_FORMATTER
 gl.xformatter = LONGITUDE_FORMATTER
 
+shapename = 'admin_1_states_provinces_lakes_shp'
+states_shp = shpreader.natural_earth(resolution='110m',
+                                     category='cultural', name=shapename)
+oilproduction = {
+    'New Jersey':  438.00,
+    'Rhode Island':   387.35,
+    'Utah': 9999
+}
+
+cmap = plt.cm.jet
+
+facecolor = {key: cmap(value/10000) for key, value in oilproduction.items()}
+for astate in shpreader.Reader(states_shp).records():
+    state_name = astate.attributes['name']
+    if state_name in oilproduction:
+        ax.add_geometries([astate.geometry], ccrs.PlateCarree(),
+                          facecolor=facecolor[state_name], alpha=0.5)
+
+vmin = 1500  # exclude all value <1500, but the aim is to exclude 0
+vmax = 2000
+levels = np.linspace(vmin, vmax, 20)
+contourf_ = ax.contourf(df['longitude'],
+                        df['latitude'],
+                        df['xch4_biascorrected_qa_surf_albedo_filtered'],
+                        cmap='jet', vmin=vmin, vmax=vmax, levels=levels)
+# cbar = fig.colorbar(contourf_, ax=ax, orientation='horizontal', fraction=0.05)
 # %% Close file
 df.close()
