16770de26f4c8ac31c742b0eb3cfce1eb35dc199
vietle <viet.letranhoang@gmail.com>
2020-05-17 10:25:56 +0300

    added gas production for uintah, data sources

--- gas_oil_production.py
+++ gas_oil_production.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 # %%
+# Data collected from
+# https://www.eia.gov/petroleum/drilling/#tabs-summary-2
 permian_df = pd.ExcelFile('production_data/dpr-data.xlsx')
 permian = permian_df.parse('Permian Region')
 
@@ -24,15 +26,24 @@ fig.savefig('Figures/Permian_production.png')
 
 # %%
 uintah = [10632162, 12146827, 11278096, 10117371]
+uintah_gas = [246050467, 205419493, 187985990, 167405812]
+# Data collected from
+#  https://oilgas.ogm.utah.gov/oilgasweb/statistics/oil-prod-by-cnty.xhtml
+# https://oilgas.ogm.utah.gov/oilgasweb/statistics/gas-prod-by-cnty.xhtml
+
+uintah_gas.reverse()
 uintah_year = [2019, 2018, 2017, 2016]
 uintah_year = pd.to_datetime(uintah_year, format='%Y')
 
 # %%
 years = mdates.YearLocator()
 fig, ax = plt.subplots(figsize=(9, 6))
-ax.plot(uintah_year, uintah)
+ax.plot(uintah_year, uintah, c='r')
+ax1 = ax.twinx()
+ax1.plot(uintah_year, uintah_gas, c='b')
 ax.xaxis.set_major_locator(years)
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
 ax.set_title('Uintah region', size=22, weight='bold')
-ax.set_ylabel('Oil production, barrel/year')
+ax.set_ylabel('Oil production, barrel/year', c='r')
+ax1.set_ylabel('Gas production, MCF/year', c='b')
 fig.savefig('Figures/Uintah_production.png')
